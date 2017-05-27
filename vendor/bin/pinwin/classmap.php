3fefe0e5a91f790184868c45fa62d858c68c0f5d
gyhsien <hsien.kh.gy@gmail.com>
2017-05-27 14:37:18 +0800

    新增classmap 產生器，寫入composer.json

---
+++ vendor/bin/pinwin/classmap.php
@@ -0,0 +1,39 @@
+<?php
+//新增PSR4 Classmap 到 composer.json內，須在專案資料夾內操作
+//php vendor/bin/pinwin/classmap.php
+require 'vendor/autoload.php';
+$library = './vendor/pinwin';
+$composer_data = json_decode(file_get_contents('./composer.json') ,true);
+$addSettings = [];
+$libraryFoldes = new \RecursiveDirectoryIterator($library);
+
+//$composer_data['autoload']['psr-4']\\'] = [];
+
+$filter = new Zend\Filter\Word\SeparatorToCamelCase();
+$psr4 = [];
+foreach ($libraryFoldes as $folder)
+{
+    //$folder = new \FilesystemIterator();
+    $dir_name = $folder->getFilename();
+    if($dir_name!= '.' && $dir_name!= '..')
+    {
+        $namespace = "Pinwin\\".$filter->filter($dir_name)."\\";
+        if(!isset($composer_data['autoload']['psr-4'][$namespace]))
+        {
+            $psr4[$namespace] = 'vendor/pinwin/'.$dir_name.'/src';
+        }
+    }
+}
+if(count($psr4) > 0)
+{
+    $composer_data['autoload']['psr-4']+=$psr4;
+    $output = json_encode($composer_data, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+    if(file_put_contents('./composer.json', $output))
+    {
+        echo "composer.json rewrite success.\n";
+    }else{
+        echo "composer.json rewrite failed.\n";
+    }
+}
+
+echo "Done.\n";
